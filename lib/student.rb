c82daed8974729df057575d6c8d7a95120521c96
Johan Esteves <jsesteves91@gmail.com>
2017-09-24 22:21:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,7 @@ class Student
     DB[:conn].execute(sql, num).collect {|row| row}
   end
 
-  def self.first_student_in_grade_10(num)
+  def self.first_student_in_grade_10
     sql = <<-SQL
     SELECT * FROM students
     WHERE grade = 10
